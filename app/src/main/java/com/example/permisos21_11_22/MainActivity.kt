47e94cc5e253b3b5cae6d109842d6b322dbd240c
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-21 13:12:40 +0100

    Creado Proyecto: Añadido binding y funciones

--- app/src/main/java/com/example/permisos21_11_22/MainActivity.kt
+++ app/src/main/java/com/example/permisos21_11_22/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.permisos21_11_22
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.permisos21_11_22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.btnCamara.setOnClickListener {
+            checkPermisos()
+        }
+    }
+
+    private fun checkPermisos() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //No tenemos los permisos asi que hay que pedirlos
+        } else {
+            abrirCamara()
+        }
+    }
+
+    private fun abrirCamara() {
+
     }
 }
